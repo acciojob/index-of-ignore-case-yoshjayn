5a53d59db4ee4b0e0d96131f8fbda433c47cc3cd
Yashaswini Jain <122783078+yoshjayn@users.noreply.github.com>
2025-03-12 21:55:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function indexOfIgnoreCase(s1, s2) {
   // write your code here
 	    let lengthToFind = s2.length
         for(let i=0; i<s1.length; i++){
-            if(s2 == s1.substring(i,i+lengthToFind)) return i || -1
+            if(s2.toLowerCase() == s1.toLowerCase().substring(i,i+lengthToFind)) return i || -1
         }
 }
 
